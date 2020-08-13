a833096af9c6b2b203ddbf2b47e4779bc9dfa5af
martin-jakovcevic <martin_jakovcevic@yahoo.com>
2020-08-13 23:33:48 +0200

    creation of new action

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -12,7 +12,12 @@ class RecipesController < ApplicationController
   end
 
   def create
-
+    recipe = Recipe.new(recipe_params)
+    if recipe.save
+      redirect_to recipe
+    else
+      render 'new'
+    end
   end
 
   def edit
@@ -27,5 +32,9 @@ class RecipesController < ApplicationController
 
   end
 
+  private
 
+  def recipe_params
+    params.require(:recipe).permit(:title, :description, :instructions)
+  end
 end
