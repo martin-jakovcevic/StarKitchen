51b24a7868b9e7ca4f197414b0bb684a98d80435
martin-jakovcevic <martin_jakovcevic@yahoo.com>
2020-08-18 00:40:44 +0200

    created new partial for editing comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,17 +1,40 @@
 class CommentsController < ApplicationController
   def create
-    recipe = Recipe.find(params[:recipe_id])
+    @recipe = Recipe.find(params[:recipe_id])
+    @comment = @recipe.comments.build(comment_params)
 
-    comment = recipe.comments.build(comment_params)
-
-    comment.save
-    redirect_to recipe
+    if @comment.save
+      redirect_to @recipe
+    else
+      render :new
+    end
   end
 
   def destroy
     comment = Comment.find(params[:id])
     comment.destroy
     redirect_to comment.recipe
+  end
+
+  def edit
+    @comment = Comment.find(params[:id])
+    @recipe = @comment.recipe
+  end
+
+  def update
+    @comment = Comment.find(params[:id])
+    @recipe = @comment.recipe
+
+    if @comment.update comment_params
+      redirect_to @recipe
+    else
+      render :edit
+    end
+  end
+
+  def new
+    @recipe = Recipe.find(params[:recipe_id])
+    @comment = @recipe.comments.build
   end
 
   private
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,4 @@
 class Comment < ApplicationRecord
   belongs_to :recipe
+  validates :commenter, :body, presence: true
 end
