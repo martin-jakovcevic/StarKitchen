5b111dce4f812cd87e9c745e9814538448d70c5b
martin-jakovcevic <martin_jakovcevic@yahoo.com>
2020-08-18 17:48:08 +0200

    Creation of new migration index_for_users_email and validations for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,27 @@
 class User < ApplicationRecord
-  validates :name, presence: true, length: {maximum: 255}
-  validates :email, presence: true length: {maximum: 50}
+  MAXIMUM_NAME_LENGTH = 255
+  MAXIMUM_EMAIL_LENGTH = 255
+  MINIMUM_PASSWORD_LENGTH = 5
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+
+  has_secure_password
+
+  has_many :recipes, dependent: :destroy
+  has_many :comments, dependent: :destroyg
+
+  before_save :email_to_downcase
+
+  validates :name, presence: true, length: { maximum: MAXIMUM_NAME_LENGTH }
+  validates :email, presence: true, length: { maximum: MAXIMUM_EMAIL_LENGTH },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: true
+  validates :password, presence: true, length: { minimum: MINIMUM_PASSWORD_LENGTH }
+
+  private
+
+  def email_to_downcase
+    self.email = email.downcase
+  end
+
 end
