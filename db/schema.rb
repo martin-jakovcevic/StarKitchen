085b6f4957f883d5d5d916bf3dd305b3eb0c3435
martin-jakovcevic <martin_jakovcevic@yahoo.com>
2020-08-18 18:11:07 +0200

    Creation of add_password_digest_to_users migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_18_144117) do
+ActiveRecord::Schema.define(version: 2020_08_18_155048) do
 
   create_table "comments", force: :cascade do |t|
     t.string "commenter"
@@ -34,6 +34,8 @@ ActiveRecord::Schema.define(version: 2020_08_18_144117) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "comments", "recipes"
